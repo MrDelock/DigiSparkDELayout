965d2eb159d245f9d1100fe32fb8474b7cfda3bb
MrDelock <mrdelock123@gmail.com>
2018-08-07 11:16:10 +0200

    Add files via upload

---
+++ Code_DE_Layout.ino
@@ -0,0 +1,55 @@
+//This DigiKeyboard Script downloads and sets a wallpaper through the Windows PowerShell. It has been tested successfully
+//on Windows 10 and should probably work with Windows 7/8/8.1 etc.
+
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+
+  // Open PowerShell
+   
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+
+  // Set Layout to US
+  
+  DigiKeyboard.print("Set");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("WinUserLanguageList ");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("Force en");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("US");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  // Exploit
+  
+  
+  
+  // Set Layout to DE
+
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("Set-WinUserLanguageList -Force de-DE");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
